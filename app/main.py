19cd71d1eb63ac6bf2d14cf1ded953444e0e7483
Mohammed Arkam K <mohammedarkamsheriff@gmail.com>
2026-08-18 20:08:05 +0530

    Initial project structure

---
+++ app/main.py
@@ -0,0 +1,8 @@
+def main():
+    print("================================")
+    print("   SMART EXPENSE MANAGER")
+    print("================================")
+
+
+if __name__ == "__main__":
+    main()
